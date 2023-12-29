2f24fffc395b553eb0dd47082e309a7af438d873
hari1027 <hariharan.n@ozonetel.com>
2023-12-29 12:12:50 +0530

    second commit

--- index.js
+++ index.js
@@ -4,47 +4,8 @@ const http = require('http');
 const WebSocket = require('ws');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const nodemailer = require('nodemailer');
-const crypto = require('crypto');
-
 app.use(bodyParser.json());
 app.use(cors());
-
-// Email Verification server
-
-function generateToken() {
-    return crypto.randomBytes(20).toString('hex');
-}
-
-const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'hari04harry@gmail.com',
-        pass: 'kqmw kgxc cfbg qmkl',
-    },
-});
-
-app.post('/send-email', (req, res) => {
-    const { email } = req.body;
-    const token = generateToken();
-
-    const mailOptions = {
-        from: 'hari04harry@gmail.com',
-        to: email,
-        subject: 'Email Verification',
-        text: `your token is : ${token}`,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error(error);
-            res.status(500).send({ message: "Error sending email" });
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.status(200).send({ message: "Email sent", token: token });
-        }
-    });
-});
 
 // room server
 
