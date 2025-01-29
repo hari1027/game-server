6db1db71aa3d33652496091eff6939e691a4117b
hari1027 <hari04harry@gmail.com>
2025-01-29 11:08:33 +0530

    deleted vercel and added dotenv

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const http = require('http');
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 app.use(express.json({ type: "application/json" }));
 app.use(cors());
@@ -123,6 +126,6 @@ app.delete('/delete-room/:roomId', (req, res) => {
     res.json({ message: `Room ${roomId} has been deleted, and all participants have been removed.` });
 });
 
-server.listen(5000, () => {
-    console.log('WebSocket server listening on port 5000');
+server.listen(process.env.PORT, () => {
+    console.log(`WebSocket server listening on port ${process.env.PORT}`);
 });
